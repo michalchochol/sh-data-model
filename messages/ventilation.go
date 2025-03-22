a9e13cc561886026a7d1ba512518f18bff92e992
mchochol <m.chochol@dotone.pl>
2025-03-22 12:56:00 +0100

    initial model

---
+++ messages/ventilation.go
@@ -0,0 +1,18 @@
+package messages
+
+// type Ventilation struct {
+// 	Data    VentilationData
+// 	Control VentilationControl
+// }
+
+// type VentilationData struct {
+//  ExternalTemp string
+// }
+
+// type VentilationControl struct {
+// }
+
+const (
+	VENT_DATA_EXTERNAL_TEMP = "ventilation/data/ExternalTemp"
+	VENT_DATA_ENERGY_USED   = "ventilation/data/EnergyUsed"
+)
